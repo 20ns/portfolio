b617b90c7d4a946a28b70e2e5fdc35cc7111c9fa
20Ns <230090446@aston.ac.uk>
2024-12-31 00:18:52 +0000

    made sure that the footer was actually in the footer location and that the website was organised

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,23 @@ import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
+// Import About and Skills components when you create them:
+// import About from './components/About';
+// import Skills from './components/Skills';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-      <Hero />
-      <Projects />
-      <Contact />
-    </div>
+      <main>
+        <section id="hero"><Hero /></section>
+        {/* Render About and Skills components when you create them: */}
+        {/* <section id="about"><About /></section> */}
+        {/* <section id="skills"><Skills /></section> */}
+        <section id="portfolio"><Projects /></section>
+        <section id="contact"><Contact /></section>
+      </main>
+    </>
   );
 }
 
